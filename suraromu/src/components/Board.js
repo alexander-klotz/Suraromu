944fafdcf77c931448d78346c1d3ab00a03c48e2
csaz3941 <csaz3941@student.uibk.ac.at>
2023-05-12 22:39:29 +0200

    fixed warning messages

--- suraromu/src/components/Board.js
+++ suraromu/src/components/Board.js
@@ -4,9 +4,6 @@ import Grid from './Grid'
 
 export default function Board(props) {
   
-  const rows = props.rows
-  const columns = props.columns
-  
   return (
     <div className="board">
       <Grid {...props}/>
