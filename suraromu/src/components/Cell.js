5c907ac99ab2c472f9de8e2ae016788499552e45
csaz3941 <csaz3941@student.uibk.ac.at>
2023-12-03 23:30:34 +0100

    made more performant

--- suraromu/src/components/Cell.js
+++ suraromu/src/components/Cell.js
@@ -1,82 +1,49 @@
-import React, { useRef, useEffect } from 'react';
-import $ from 'jquery';
+import React, { useRef, useEffect} from 'react';
+
 
 export default function Cell(props) {
 
     const outerDivRef = useRef(null);
     let content = "";
-
+    
+    
+    // TODO: maybe make this fixed by calculating it in the Grid.js
     useEffect(() => {
-        const outerDiv = $(outerDivRef.current);
-        const pElement = outerDiv.find('p');
-
+        const outerDiv = outerDivRef.current;
+        const pElement = outerDiv.querySelector('p');
         const updateFontSize = () => {
-            const fontSize = outerDiv.height();
-            pElement.css('font-size', fontSize*0.7);
+            const fontSize = outerDiv.offsetHeight;
+            pElement.style.fontSize = `${fontSize * 0.6}px`;
         };
-
-        // Initial font-size update
         updateFontSize();
 
-        // ResizeObserver
         const resizeObserver = new ResizeObserver(updateFontSize);
-        resizeObserver.observe(outerDiv[0]);
+        resizeObserver.observe(outerDiv);
 
-        // Clean up the ResizeObserver on unmount
         return () => {
-            resizeObserver.unobserve(outerDiv[0]);
+            resizeObserver.unobserve(outerDiv);
             resizeObserver.disconnect();
         };
     }, []);
-
+    
 
     // Type: normal=0 blocked=1 gateHori=2 gateVert=3 gateNumber=4 Start=5
     const classNames = ["cell", "blockedCell", "gateHoriCell", "gateVertCell", "gateNumberCell", "startCell"]
     const idx2D = [Math.floor(props.index/(props.puzzle.cols)), props.index%(props.puzzle.cols)]
-
-    let vertiGateIdxs = []
-    let horiGateIdxs = []
-    for (let key in props.puzzle.gates) {
-        if (key === '0') {
-            // all the unordered gates
-            for (let gateKey in props.puzzle.gates[key]) {
-                const curGate = props.puzzle.gates[key][gateKey]
-                const length = curGate.length
-                if(curGate.orientation === "h"){
-                    horiGateIdxs = horiGateIdxs.concat(Array.from({ length }, (_, index) => [curGate.startCell[0], curGate.startCell[1] + index]))
-                }
-                if(curGate.orientation === "v"){
-                    vertiGateIdxs = vertiGateIdxs.concat(Array.from({ length }, (_, index) => [curGate.startCell[0] + index, curGate.startCell[1]]))
-                }
-
-            }
-        } else {
-            // all the ordered cells
-            const curGate = props.puzzle.gates[key]
-            const length = curGate.length
-            if(curGate.orientation === "h"){
-                horiGateIdxs = horiGateIdxs.concat(Array.from({ length }, (_, index) => [curGate.startCell[0], curGate.startCell[1] + index]))
-            }
-            if(curGate.orientation === "v"){
-                vertiGateIdxs = vertiGateIdxs.concat(Array.from({ length }, (_, index) => [curGate.startCell[0] + index, curGate.startCell[1]]))
-            }
-            
-        }
-    }
-
+    
 
     function getCellType(idx) {
         let type = 0
 
-        if (props.puzzle.blockedCells.some(cell => JSON.stringify(cell) === JSON.stringify(idx))) {
+        if (props.puzzle.blockedCells.some(cell => cell.length === idx.length && cell.every((value, index) => value === idx[index]))) {
             type = 1;
         } else if (props.puzzle.startCell[0] === idx[0] && props.puzzle.startCell[1] === idx[1]) {
             type = 5;
             content = countGates(props.puzzle.gates).toString();
-        }else if (vertiGateIdxs.some(cell => JSON.stringify(cell) === JSON.stringify(idx))) {
+        } else if (props.vertiGateIdxs.some(cell => cell.length === idx.length && cell.every((value, index) => value === idx[index]))) {
             type = 3;
-        }else if (horiGateIdxs.some(cell => JSON.stringify(cell) === JSON.stringify(idx))) {
-                type = 2;
+        } else if (props.horiGateIdxs.some(cell => cell.length === idx.length && cell.every((value, index) => value === idx[index]))) {
+            type = 2;
         } else {
             switch(gateNumberCell(idx)) {
                 case "0":
@@ -90,48 +57,14 @@ export default function Cell(props) {
                     break;
             }
         }
-
+        
         return type
     }
 
     // returns string == blocked gate cell numbering ("0" for unnumbered and "" if it's a non gateNumberCell)
     function gateNumberCell(idx) {
-        for (let key in props.puzzle.gates) {
-            if (key === '0') {
-                // all the unordered gates
-                for (let gateKey in props.puzzle.gates[key]) {
-                    const possibleGateCells = getBlockedGateCells(props.puzzle.gates[key][gateKey])
-                    if(possibleGateCells.some(cell => JSON.stringify(cell) === JSON.stringify(idx))) {
-                        return "0";
-                    }
-                }
-            } else {
-                // all the ordered cells
-                const possibleGateCells = getBlockedGateCells(props.puzzle.gates[key])
-                if(possibleGateCells.some(cell => JSON.stringify(cell) === JSON.stringify(idx))) {
-                    return key.toString();
-                }
-            }
-        }
-        return ""
+        return props.gateGrid[idx[0]][idx[1]]
     }
-
-    function getBlockedGateCells(gate) {
-        const cells = []
-        const row = gate.startCell[0]
-        const col = gate.startCell[1]
-
-        if(gate.orientation === "h") {
-            cells.push([row, col-1])
-            cells.push([row, col+gate.length])
-        }else {
-            cells.push([row-1, col])
-            cells.push([row+gate.length, col])
-        }
-
-        return cells
-    }
-
 
     function countGates(gatesObject) {
         let count = 0;
